3418f84a8f48d77ae74236872507ea7d7c58eaad
jiro <gitromaru666>
2016-11-19 14:52:12 +0900

    ver1.0のアップロード

---
+++ src/jiro/lib/javafx/stage/DirectoryChooserManager.java
@@ -0,0 +1,51 @@
+package jiro.lib.javafx.stage;
+
+import java.io.File;
+import java.util.Optional;
+
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+/**
+ * DirectoryChooserで開いたディレクトリのパスを保持するクラス。
+ * @author jiro
+ * @version 1.0
+ */
+public class DirectoryChooserManager {
+  private File currentDirectory;
+  private final DirectoryChooser dc;
+
+  /**
+   * カレントディレクトリをDirectoryChooserの初期ディレクトリに設定するコンストラクタ。
+   */
+  public DirectoryChooserManager() {
+    this(null);
+  }
+
+  /**
+   * ファイルパスを指定するコンストラクタ
+   * @param dirPath ファイルパス
+   */
+  public DirectoryChooserManager(String dirPath) {
+    dc = new DirectoryChooser();
+    if (dirPath != null) dc.setInitialDirectory(new File(dirPath));
+    else dc.setInitialDirectory(new File("."));
+  }
+
+  /**
+   * ディレクトリ選択画面を開く。
+   * 次回起動時はその選択したディレクトリを再び選択できるように
+   * 初期ディレクトリが更新される。
+   * @param stage Stage
+   * @return 取得したディレクトリ
+   */
+  public Optional<File> openDirectory(Stage stage) {
+    File dir = dc.showDialog(stage);
+    Optional<File> dirOpt = Optional.ofNullable(dir);
+    dirOpt.ifPresent(f -> {
+      currentDirectory = f.getParentFile();
+      dc.setInitialDirectory(currentDirectory);
+    });
+    return dirOpt;
+  }
+}
